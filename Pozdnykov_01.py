6b66fa38d1dc4ad1898d4853ae21849f0a800e98
Alexsandr-STARosta <79752062+Alexsandr-STARosta@users.noreply.github.com>
2021-02-27 22:26:02 +0700

    Delete Pozdnykov_01.py

--- Pozdnykov_01.py
+++
@@ -1,9 +0,0 @@
-#Задание 1
-import random
-n=100
-my_file = open("numbers.csv","w+")
-for number in range(0,n):
-    my_file.write(str(random.randint(1,100)))
-    if(number!=n-1):
-        my_file.write(",")   
-my_file.close()
